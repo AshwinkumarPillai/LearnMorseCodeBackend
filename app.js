833525be23ae25e8e2d60750561bfa9fe4dcc54c
Ashwin <ashwinfury1@gmail.com>
2024-12-07 23:35:23 -0500

    schedule pinger api route added

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use(cors());
 const loginRoutes = require("./routes/user.routes");
 
 app.use(loginRoutes);
+app.use("/schedule-pinger", (req, res) => {
+  res.send("Thank you for keeping me alive pinger!\n-Learn Morse Code");
+});
 
 let MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3100;
